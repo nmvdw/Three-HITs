7ca6cd5386b124889e16c5d8b0288693cfa5487e
Andrej Bauer <Andrej.Bauer@andrej.com>
2017-03-31 15:18:01 +0200

    Allow many point constructors, reorganize.

--- Formalization/is_hit.v
+++ Formalization/is_hit.v
@@ -6,13 +6,24 @@ Axiom cheating : forall A : Type, A.
 Ltac todo := apply cheating.
 Axiom magic : forall A B, A -> B.
 
+Section Endpoints.
 (* The definition of a HIT signature and a HIT for the signature. *)
 
-(* An endpoint is also called a "constructor term". *)
-Inductive endpoint {S : polynomial} : polynomial -> polynomial -> Type :=
+(* An endpoint is also called a "constructor term". It is used in
+   the HIT signature to describe the endpoints of path constructors. *)
+
+(*
+   Endpoints are parameterized by a family of polynomials which will
+   be specialized to the point constructors in the definition of HITS.
+*)
+Variable I: Type.
+Variable C : I -> polynomial.
+
+Inductive endpoint :
+  polynomial -> polynomial -> Type :=
   | endpoint_var : forall {P}, endpoint P P
-  | endpoint_const : forall {P} {C : Type}, C -> endpoint P (poly_const C)
-  | endpoint_constr : forall {P}, endpoint P S -> endpoint P poly_var
+  | endpoint_const : forall {P} {T : Type}, T -> endpoint P (poly_const T)
+  | endpoint_constr : forall {P} (i : I), endpoint P (C i) -> endpoint P poly_var
   | endpoint_fst : forall {P Q R}, endpoint P (poly_times Q R) -> endpoint P Q
   | endpoint_snd : forall {P Q R}, endpoint P (poly_times Q R) -> endpoint P R
   | endpoint_pair : forall {P Q R}, endpoint P Q -> endpoint P R -> endpoint P (poly_times Q R)
@@ -20,12 +31,12 @@ Inductive endpoint {S : polynomial} : polynomial -> polynomial -> Type :=
   | endpoint_inr : forall {P Q R}, endpoint P R -> endpoint P (poly_plus Q R)
 .
 
-(* Endpoint acting on elements. *)
+(* Endpoint acting on data for point constructors. *)
 Fixpoint endpoint_act
-  {S P Q : polynomial}
-  {A : Type}
-  (c : poly_act S A -> A)
-  (e : @endpoint S P Q)
+  {P Q : polynomial}
+  {A : Type} (* carrier *)
+  (c : forall i, poly_act (C i) A -> A) (* point constructors *)
+  (e : endpoint P Q) (* the endpoint *)
   {struct e}
   : poly_act P A -> poly_act Q A.
 Proof.
@@ -35,130 +46,46 @@ Proof.
   - exact (fun u => u).
 
   (* endpoint_const *)
-  - exact (fun _ => c0).
+  - exact (fun _ => t).
 
   (* endpoint_constr *)
-  - exact (fun u => c (endpoint_act _ _ _ _ c e u)).
+  - exact (fun u => c i (endpoint_act _ _ _ c e u)).
 
   (* endpoint_fst *)
-  - exact (fun u => fst (endpoint_act _ _ _ _ c e u)).
+  - exact (fun u => fst (endpoint_act _ _ _ c e u)).
 
   (* endpoint_snd *)
-  -  exact (fun u => snd (endpoint_act _ _ _ _ c e u)).
+  -  exact (fun u => snd (endpoint_act _ _ _ c e u)).
 
   (* endpoint_pair *)
-  - exact  (fun u => (endpoint_act _ _ _ _ c e1 u, endpoint_act _ _ _ _ c e2 u)).
+  - exact  (fun u => (endpoint_act _ _ _ c e1 u, endpoint_act _ _ _ c e2 u)).
 
   (* endpoint_inl *)
-  -  exact (fun u => inl (endpoint_act _ _ _ _ c e u)).
+  -  exact (fun u => inl (endpoint_act _ _ _ c e u)).
 
   (* endpoint_inr *)
-  -  exact (fun u => inr (endpoint_act _ _ _ _ c e u)).
+  -  exact (fun u => inr (endpoint_act _ _ _ c e u)).
 Defined.
 
-(* A HIT is specified by a single point constructor and a family of path constructors.
-   Note: having a single point constructor is as general as having many.
-   Note: we are allowing an arbitrary family of path construcrors, and later we will
-         require that there is a bound on how deeply the point constructors are nested.
-*)
-Structure hit_signature := {
-  hit_point : polynomial ; (* the signature for a single point constructor *)
-  hit_paths_index : Type ; (* indexing for path constructors *)
-  hit_paths_param : hit_paths_index -> polynomial ; (* the parameters of each path constructor *)
-  hit_paths_lhs : forall i, @endpoint hit_point (hit_paths_param i) poly_var ; (* the left endpoint *)
-  hit_paths_rhs : forall i, @endpoint hit_point (hit_paths_param i) poly_var ; (* the right endpoint *)
-}.
-
-(* Example: propositional truncation *)
-Definition trunc_signature (A : Type) :=
-  {|
-    hit_point := poly_const A ;
-    hit_paths_index := Unit ;
-    hit_paths_param := (fun _ => poly_times poly_var poly_var) ;
-    hit_paths_lhs := (fun _ => endpoint_fst endpoint_var) ;
-    hit_paths_rhs := (fun _ => endpoint_snd endpoint_var)
-  |}.
-
-(* Example: circle *)
-
-Definition circle_signature :=
-  {|
-    hit_point := poly_const Unit ;
-    hit_paths_index := Unit ;
-    hit_paths_param := (fun _ => poly_const Unit) ;
-    hit_paths_lhs := (fun _ => endpoint_constr endpoint_var) ;
-    hit_paths_rhs := (fun _ => endpoint_constr endpoint_var)
-  |}.
-
-(* Example: susupension *)
-
-Definition suspension_signature (A : Type) :=
-  {|
-    hit_point := poly_plus (poly_const Unit) (poly_const Unit) ;
-    hit_paths_index := Unit ;
-    hit_paths_param := (fun _ => poly_const A) ;
-    hit_paths_lhs := (fun _ => endpoint_constr (endpoint_inl (endpoint_const tt))) ;
-    hit_paths_rhs := (fun _ => endpoint_constr (endpoint_inr (endpoint_const tt)))
-  |}.
-
-(* Example: natural numbers *)
-Definition nat_signature :=
-  {|
-    hit_point := poly_plus (poly_const Unit) poly_var ;
-    hit_paths_index := Empty ;
-    hit_paths_param := Empty_rect _ ;
-    hit_paths_lhs := Empty_rect _ ;
-    hit_paths_rhs := Empty_rect _
-  |}.
-
-(* Given a carrier type [A], we want to explain what the structure
-   of a HIT on [A] is. For this purpose we need to define the types
-   of the point constructors, the paths, and intro rules, and the
-   eliminator. *)
-
-(* We now embark upon defining the elimnator for the given HIT. *)
-
-Structure hit_data (Σ : hit_signature) :=
-{
-  data_carrier :> Type ; (* the carrier type *)
-  data_point : poly_act (hit_point Σ) data_carrier -> data_carrier ; (* the point constructor *)
-  data_eq := (* the type of a path constructor parameterized by [Q] with endpoints [e1] and [e2] *)
-    (fun (Q : polynomial)
-       (e1 e2 : endpoint Q poly_var) =>
-       forall (u : poly_act Q data_carrier), endpoint_act data_point e1 u = endpoint_act data_point e2 u) ;
-  data_paths : forall i u, endpoint_act data_point (hit_paths_lhs Σ i) u = endpoint_act data_point (hit_paths_rhs Σ i) u
-}.
-
-Arguments data_point {_} _ _.
-Arguments data_paths {_} _ _ _.
-
-(* Example: the data for nat. *)
-Definition nat_data : hit_data nat_signature.
-Proof.
-  simple refine {| data_carrier := nat |}.
-  - exact (fun u => match u with inl _ => 0 | inr n => S n end).
-  - intro i ; elim i.
-Defined.
-
-(* The lift of an endpoint -- [dact] stands for "dependent action". *)
-
+(* The dependent action of an endpoint, this is used for
+   "lifting" the path constructors in the elimnation principle. *)
 Fixpoint endpoint_dact
-         {P Q R : polynomial}
+         {Q R : polynomial}
          {A : Type}
-         (c : poly_act P A -> A)
+         (c : forall i, poly_act (C i) A -> A)
          (F : A -> Type)
-         (f : forall u : poly_act P A, poly_fam P F u -> F (c u))
-         (e : @endpoint P Q R)
+         (f : forall (i : I) (u : poly_act (C i) A), poly_fam (C i) F u -> F (c i u))
+         (e : @endpoint Q R)
          {struct e} :
   forall (x : poly_act Q A) (h : poly_fam Q F x), poly_fam R F (endpoint_act c e x).
 Proof.
   simple refine (
-           match e as z in @endpoint _ Q R
+           match e as z in @endpoint Q R
                  return (forall (x : poly_act Q A) (h : poly_fam Q F x), poly_fam R F (endpoint_act c z x))
            with
            | endpoint_var _ => _
            | endpoint_const _ _ t => _
-           | endpoint_constr _ u => _
+           | endpoint_constr _ i u => _
            | endpoint_fst _ _ _ u => _
            | endpoint_snd _ _ _ u => _
            | endpoint_pair _ _ _ u v => _
@@ -222,64 +149,163 @@ Proof.
     }
 Defined.
 
-Definition hit_ind {Σ : hit_signature} (H : hit_data Σ) :=
-  forall (F : H -> Type)
-    (c : forall u : poly_act (hit_point Σ) H, poly_fam (hit_point Σ) F u -> F (data_point H u))
-    (p : forall (i : hit_paths_index Σ)
-           (x : poly_act (hit_paths_param Σ i) H)
-           (h : poly_fam (hit_paths_param Σ i) F x),
-        transport _ (data_paths H i x) (endpoint_dact (data_point H) F c (hit_paths_lhs Σ i) x h) =
-        endpoint_dact (data_point H) F c (hit_paths_rhs Σ i) x h
-        ),
-    (forall x : H, F x).
+End Endpoints.
 
+Arguments endpoint {_} _ _ _.
+Arguments endpoint_var {_ _} {_}.
+Arguments endpoint_const {_ _} {_} {_} _.
+Arguments endpoint_constr {_ _} {_} _ _ .
+Arguments endpoint_fst {_ _} {_ _ _} _.
+Arguments endpoint_snd {_ _} {_ _ _} _.
+Arguments endpoint_pair {_ _} {_ _ _} _ _.
+Arguments endpoint_inl {_ _} {_ _ _} _.
+Arguments endpoint_inr {_ _} {_ _ _} _.
 
-(* Example: we prove that natural numbers have an eliminator. *)
-Lemma nat_ind : hit_ind nat_data.
+Arguments endpoint_act {_ _} {_ _ _} _ _ _.
+Arguments endpoint_dact {_ _} {_ _ _} _ _ _ _ _ _.
+
+(* A HIT signature is specified by point constructors and path constructors.
+
+   Note: we are allowing an arbitrary family of poitn and path construcrors.
+         We will restrict them later on in existence theorem.
+*)
+Structure hit_signature := {
+  (* indexing for point constructors *)
+  sig_point_index : Type ;
+
+  (* the signatures for point constructors *)
+  sig_point : sig_point_index -> polynomial ;
+
+  (* indexing for path constructors *)
+  sig_path_index : Type ;
+
+  (* the parameters of each path constructor *)
+  sig_path_param : sig_path_index -> polynomial ;
+
+  (* the left and right endpoints of path constructors *)
+  sig_path_lhs : forall i, endpoint sig_point (sig_path_param i) poly_var ;
+  sig_path_rhs : forall i, endpoint sig_point (sig_path_param i) poly_var
+}.
+
+(* Example: propositional truncation *)
+Definition trunc_signature (A : Type) :=
+  {|
+    sig_point_index := Unit ;
+    sig_point := (fun _ => poly_const A) ;
+    sig_path_index := Unit ;
+    sig_path_param := (fun _ => poly_times poly_var poly_var) ;
+    sig_path_lhs := (fun _ => endpoint_fst endpoint_var) ;
+    sig_path_rhs := (fun _ => endpoint_snd endpoint_var)
+  |}.
+
+(* Example: circle *)
+
+Definition circle_signature :=
+  {|
+    sig_point_index := Unit ;
+    sig_point := (fun _ => poly_const Unit) ;
+    sig_path_index := Unit ;
+    sig_path_param := (fun _ => poly_const Unit) ;
+    sig_path_lhs := (fun _ => endpoint_constr tt endpoint_var) ;
+    sig_path_rhs := (fun _ => endpoint_constr tt endpoint_var)
+  |}.
+
+(* Example: susupension *)
+
+Definition suspension_signature (A : Type) :=
+  {|
+    sig_point_index := Bool ;
+    sig_point := (fun _ => poly_const Unit) ;
+    sig_path_index := Unit ;
+    sig_path_param := (fun _ => poly_const A) ;
+    sig_path_lhs := (fun _ => endpoint_constr false (endpoint_const tt)) ;
+    sig_path_rhs := (fun _ => endpoint_constr true (endpoint_const tt))
+  |}.
+
+(* Example: natural numbers *)
+Definition nat_signature :=
+  {|
+    sig_point_index := Bool ;
+    sig_point := (fun b => if b then poly_const Unit else poly_var) ;
+    sig_path_index := Empty ;
+    sig_path_param := Empty_rect _ ;
+    sig_path_lhs := Empty_rect _ ;
+    sig_path_rhs := Empty_rect _
+  |}.
+
+(* Given a carrier type [A], we want to explain what the structure
+   of a HIT on [A] is. For this purpose we need to define the types
+   of the point constructors, the paths, and intro rules, and the
+   eliminator. *)
+
+(* We now define what a HIT is. *)
+
+Structure HIT (Σ : hit_signature) :=
+{
+  (* the carrier of the HIT *)
+  hit_carrier :> Type ;
+
+  (* the point constructor *)
+  hit_point : forall i, poly_act (sig_point Σ i) hit_carrier -> hit_carrier ;
+
+  (* path constructors *)
+  hit_path : forall i u, endpoint_act hit_point (sig_path_lhs Σ i) u =
+                    endpoint_act hit_point (sig_path_rhs Σ i) u ;
+
+  (* the elinator *)
+  hit_ind :
+    forall (F : hit_carrier -> Type)
+      (c : forall (i : sig_point_index Σ) (u : poly_act (sig_point Σ i) hit_carrier),
+          poly_fam (sig_point Σ i) F u -> F (hit_point i u))
+      (p : forall (i : sig_path_index Σ)
+             (x : poly_act (sig_path_param Σ i) hit_carrier)
+             (h : poly_fam (sig_path_param Σ i) F x),
+               transport _ (hit_path i x)
+               (endpoint_dact hit_point F c (sig_path_lhs Σ i) x h) =
+               endpoint_dact hit_point F c (sig_path_rhs Σ i) x h)
+      (x : hit_carrier),
+      F x
+}.
+
+Arguments hit_point {_ _} _ _.
+Arguments hit_path {_ _} _ _.
+Arguments hit_ind {_ _} _ _ _ _.
+
+(* Example: nat is a hit. *)
+Definition nat_hit : HIT nat_signature.
 Proof.
-  intros F c p x.
-  simpl in *.
-  induction x as [| n y].
-  - now apply (c (inl tt)).
-  - apply (c (inr n)).
-    exact y.
+  simple refine {| hit_carrier := nat |}.
+  - intros [ | ].
+    + intros _ ; exact 0.
+    + exact S.
+  - intro i ; elim i.
+  - intros F c p x.
+    induction x as [| n y].
+    + exact (c true tt tt).
+    + apply (c false n y).
 Defined.
+
 
 (* Example: the HoTT library circle has an eliminator. *)
-Definition circle_data : hit_data circle_signature.
+Definition circle_hit : HIT circle_signature.
 Proof.
-  refine {| data_carrier := S1 ;
-            data_point := (fun _ => base) ;
-            data_paths := (fun _ _ => _ ) |}.
-  exact loop.
-Defined.
-
-Lemma circle_ind : hit_ind circle_data.
-Proof.
-  intros F c f x.
-  apply (S1_ind F (c tt tt)), (f tt).
+  simple refine {| hit_carrier := S1 ;
+                   hit_point := (fun _ _ => base) ;
+                   hit_path := (fun _ _ => _ ) |}.
+  - exact loop.
+  - intros F c f x.
+    now apply (S1_ind F (c tt tt tt)), (f tt).
 Defined.
 
 (* Example: the HoTT library suspension has an eliminator. *)
 
-Definition suspension_data (A : Type) : hit_data (suspension_signature A).
+Definition suspension_hit (A : Type) : HIT (suspension_signature A).
 Proof.
-  simple refine {| data_carrier := Susp A |}.
-  - intros [_|_] ; [exact North | exact South].
-  - intros [] x.
-    exact (merid x).
+  simple refine {| hit_carrier := Susp A |}.
+  - intros [_|_] ; [exact South | exact North].
+  - intros [] x ; exact (merid x).
+  - intros F c f x.
+    apply (Susp_ind F (c false tt tt) (c true tt tt)).
+    intro a.
+    apply (f tt a a).
 Defined.
-
-Lemma supsension_ind (A : Type) : hit_ind (suspension_data A).
-Proof.
-  intros F c f x.
-  simpl in *.
-  apply (Susp_ind F (c (inl tt) tt) (c (inr tt) tt)).
-  intro a.
-  apply (f tt a a).
-Defined.
-
-(* The computation rule for points. *)
-
-
-End HIT_structure.
