301535653f58842ddb90fd5644183d96c6294312
Andrej Bauer <Andrej.Bauer@andrej.com>
2017-04-02 00:34:11 +0200

    Point out that we need extra conditions on the main theorem.

--- Formalization/hit_existence.v
+++ Formalization/hit_existence.v
@@ -6,14 +6,14 @@ Require Import hit_structure.
 Require Import colim.
 
 Theorem HIT_existence (Σ : hit_signature) : HIT Σ.
+  (* Well, we presumably need to state some additional assumptions about [Σ] so that the
+     nesting of point constructors is bounded. *)
+
 Proof.
-  (* here we should construct the hit in question using
-     only the "three" hits. These we will take from the
-     HoTT library, so that we get judgmental computation
-     rules for points. The alternative would be to assume
-     that the theorem holds for certain specific instances
-     of [Σ] and prove that all the others follow. But that
-     would likely be quite a bit more technically involved.
-  *)
+  (* here we should construct the hit in question using only the "three" hits. These we
+     will take from the HoTT library, so that we get judgmental computation rules for
+     points. The alternative would be to assume that the theorem holds for certain
+     specific instances of [Σ] and prove that all the others follow. But that would likely
+     be quite a bit more technically involved. *)
   admit.
 Admitted.
