0275c9e3879bd3c781b3ab4a93bdb7695de6538d
Niels <nmvdw@ip-145-116-143-67.wlan-int.ru.nl>
2017-04-05 14:59:13 +0200

    Further work on colimits. Finished colimit of constant sequence.

--- Formalization/colim.v
+++ Formalization/colim.v
@@ -16,20 +16,88 @@ Require Import HoTT.HIT.Colimits.
    seeral pieces.
 *)
 
+(* In order to nicely work with colimits over the natural numbers, we want to abbreviate some principles.
+   These principles are closer to what was written in the paper, and follow from the more general rules.
+*)
+
 Definition colim_N (F : nat -> Type) (f : forall n : nat, F n -> F(S n)) : Type :=
   colimit (Build_sequence F f).
 
 Definition inc (F : nat -> Type) (f : forall n : nat, F n -> F(S n)) (n : nat) (x : F n) : colim_N F f
   := @colim mappingtelescope_graph (Build_sequence F f) n x.
 
-Definition com {F : nat -> Type} {f : forall n : nat, F n -> F(S n)} (n : nat) (x : F n) :
-  inc F f n x = inc F f (S n) (f n x).
+Definition com (F : nat -> Type) (f : forall n : nat, F n -> F(S n)) (n : nat) (x : F n) :
+  inc F f (S n) (f n x) = inc F f n x.
 Proof.
 compute.
-symmetry.
 pose (@colimp mappingtelescope_graph (Build_sequence F f) n (S n) (reflexivity (S n)) x).
 compute in p.
-apply p.
+eapply p.
+Defined.
+
+Definition cocone_P 
+  (P : Type)
+  (F : nat -> Type)
+  (f : forall (n : nat), F n -> F(S n))
+  (Pi : forall (n : nat), F n -> P)
+  (Pc : forall (n : nat) (x : F n), Pi (S n) (f n x) = Pi n x)
+  : cocone (Build_sequence F f) P.
+Proof.
+simple refine (Build_cocone _ _).
+- eapply Pi.
+
+- cbn.
+  intros.
+  induction g.
+  eapply Pc.
+Defined.
+
+Definition colim_N_rec
+  (P : Type)
+  (F : nat -> Type)
+  (f : forall (n : nat), F n -> F(S n))
+  (Pi : forall (n : nat), F n -> P)
+  (Pc : forall (n : nat) (x : F n), Pi (S n) (f n x) = Pi n x) :
+  colim_N F f -> P
+  := @colimit_rec mappingtelescope_graph (Build_sequence F f) P (cocone_P P F f Pi Pc).
+
+Definition colim_N_rec_beta_com
+  (P : Type)
+  (F : nat -> Type)
+  (f : forall (n : nat), F n -> F(S n))
+  (Pi : forall (n : nat), F n -> P)
+  (Pc : forall (n : nat) (x : F n), Pi (S n) (f n x) = Pi n x)
+  (n : nat)
+  (x : F n) :
+  ap (colim_N_rec P F f Pi Pc) (com F f n x) = Pc n x.
+Proof.
+assert
+ (ap
+    (@colimit_rec mappingtelescope_graph (Build_sequence F f) P
+       (cocone_P P F f Pi Pc))
+    (@colimp mappingtelescope_graph (Build_sequence F f) n n.+1
+       (reflexivity n.+1) x) =
+  qq (cocone_P P F f Pi Pc) n n.+1 (reflexivity n.+1) x).
+- apply (colimit_rec_beta_colimp P (cocone_P P F f Pi Pc) n 
+     (S n) (reflexivity (S n)) x).
+
+- cbn in X.
+  apply X.
+Defined.
+
+Definition colim_N_ind
+  (F : nat -> Type)
+  (f : forall (n : nat), F n -> F(S n))
+  (P : colim_N F f -> Type)
+  (Pi : forall (n : nat), forall (x : F n), P (inc F f n x))
+  (Pc : forall (n : nat) (x : F n), (com F f n x) # Pi (S n) (f n x) = Pi n x) :
+  forall (x : colim_N F f), P x.
+Proof.
+simple refine (colimit_ind _ _ _) ; cbn.
+- apply Pi.
+- intros.
+  induction g.
+  apply Pc.
 Defined.
 
 (*
@@ -38,42 +106,68 @@ Lemma 10 in paper.
 *)
 Section ColimConst.
 
+Variable A : Type.
+Definition c_fam := fun (_ : nat) => A.
+Definition c_map := fun (_ : nat) => fun (x : A) => x.
+
 (*
 colim A id -> A
 Defined by F(n) -> A as id.
 *)
-Definition CC_A (A : Type) :
-  colim_N (fun (_ : nat) => A) (fun (_ : nat) => idmap) -> A.
+Definition CC_A :
+  colim_N c_fam c_map -> A.
 Proof.
-refine (colimit_rec _ _).
-simple refine (Build_cocone _ _).
+Proof.
+simple refine (colim_N_rec _ _ _ _ _).
+- intro n.
+  apply idmap.
 - cbn.
   intros.
-  apply X.
-
-- cbn.
-  intros.
-  induction g.
   reflexivity.
 Defined.
 
-Definition A_CC (A : Type) (a : A) : colim_N (fun (_ : nat) => A) (fun (_ : nat) => idmap) :=
-  inc (fun (_ : nat) => A) (fun (_ : nat) => idmap) 0 a.
+Definition A_CC (a : A) : colim_N c_fam c_map :=
+  inc c_fam c_map 0 a.
 
-Definition iso_CC_A (A : Type) (x : A) :
-  CC_A A (A_CC A x) = x := reflexivity x.
+Definition iso_CC_A (x : A) :
+  CC_A (A_CC x) = x := reflexivity x.
 
-Definition iso_A_CC (A : Type) :
-  forall (x : colim_N (fun (_ : nat) => A) (fun (_ : nat) => idmap)), A_CC A (CC_A A x) = x.
+Definition iso_A_CC :
+  forall (x : colim_N c_fam c_map), A_CC (CC_A x) = x.
 Proof.
-simple refine (colimit_ind _ _ _).
+simple refine (colim_N_ind _ _ _ _ _).
 - cbn.
   intros n x.
   induction n.
   * reflexivity.
-  * rewrite IHn.
-    pose (com n x: inc (fun (_ : nat) => A) (fun (_ : nat) => idmap) n x  = inc (fun (_ : nat) => A) (fun (_ : nat) => idmap) (S n) x).
-    compute in p.
-    apply p.
-admit.
-Admitted.
+  * apply (IHn x @ (com c_fam c_map n x)^).
+- cbn.
+  intros.
+  rewrite @HoTT.Types.Paths.transport_paths_FlFr.
+  rewrite ap_compose.
+  rewrite ap_idmap.
+  hott_simpl.
+  rewrite ap_V.
+  unfold CC_A.
+  rewrite (colim_N_rec_beta_com A c_fam c_map).
+  hott_simpl.
+Defined.
+
+Theorem BiInv_CC_A : BiInv CC_A.
+Proof.
+split.
+- exists A_CC.
+  unfold Sect.
+  apply iso_A_CC.
+- exists A_CC.
+  unfold Sect.
+  apply iso_CC_A.
+Qed.
+
+Theorem Equiv_CC_A : IsEquiv CC_A.
+Proof.
+apply isequiv_biinv.
+apply BiInv_CC_A.
+Qed.
+
+End ColimConst.
